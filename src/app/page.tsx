7601413fb4a97ed5a26ecba33da81a1e3cf44a73
ryzen <ryzen@pop-os.localdomain>
2024-01-11 17:56:59 -0300

    Inicio

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+// app/page.tsx
+'use client'
+
+import Homepage from "../Components/Home/Homepage"
+import Dashboard from "../Components/Dash/Dashboard"
+
+export default function Page() {
+  return (
+    <div>
+        <Homepage/>
+        <Dashboard/>
+    </div>
+
+  )
+}
